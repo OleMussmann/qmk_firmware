59300d038f5dee19f9bdd2a2b8c7a8d9426bd6b7
ole <ole@isbjornlabs.com>
2019-08-03 12:38:37 +0200

    disable console

--- keyboards/planck/keymaps/OleMussmann/rules.mk
+++ keyboards/planck/keymaps/OleMussmann/rules.mk
@@ -4,4 +4,4 @@ AUDIO_ENABLE = no        # Audio output on port C6
 NKRO_ENABLE = yes        # NKey Rollover
 #AUTO_SHIFT_ENABLE = yes  # Enable shift by pressing letter keys longer
 EXTRAKEY_ENABLE = yes    # Audio control and System control
-CONSOLE_ENABLE = yes
+CONSOLE_ENABLE = no
